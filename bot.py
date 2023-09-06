81d10ba3dbdbd7acd3b0de3dfff02c57a8acf625
Ferry Schoenmakers <f.b.f.schoenmakers@gmail.com>
2023-09-06 22:25:58 +0200

    First attempt

--- bot.py
+++ bot.py
@@ -2,11 +2,18 @@ from random import choice
 from typing import List, Tuple
 
 import numpy as np
+from scipy import ndimage
 
 from ...bot import Bot
 from ...constants import Move, MOVE_VALUE_TO_DIRECTION
 from ...snake import Snake
 
+
+def gaussian_kernel(size, sigma):
+    kernel = np.zeros((size, size))
+    kernel[size//2, size//2] = 1
+    gaussian = ndimage.gaussian_filter(kernel, sigma)
+    return gaussian / gaussian[size//2, size//2]
 
 def is_on_grid(pos: np.array, grid_size: Tuple[int, int]) -> bool:
     """
@@ -15,20 +22,16 @@ def is_on_grid(pos: np.array, grid_size: Tuple[int, int]) -> bool:
     return 0 <= pos[0] < grid_size[0] and 0 <= pos[1] < grid_size[1]
 
 
-def collides(pos: np.array, snakes: List[Snake]) -> bool:
-    """
-    Check if a position is occupied by any of the snakes
-    """
-    for snake in snakes:
-        if snake.collides(pos):
-            return True
-    return False
-
-
 class FurryMuncher(Bot):
     """
     Smartness to be determined
     """
+    def __init__(self, *args, **kwargs):
+        super(FurryMuncher, self).__init__(*args, **kwargs)
+
+        self.candy_kernel = gaussian_kernel(self.grid_size[0], self.grid_size[0]/5.0)
+        self.snake_kernel = np.array([[1]])
+
 
     @property
     def name(self):
@@ -39,31 +42,46 @@ class FurryMuncher(Bot):
         return 'Ferry Schoenmakers'
 
     def determine_next_move(self, snake: Snake, other_snakes: List[Snake], candies: List[np.array]) -> Move:
-        moves = self._determine_possible_moves(snake, other_snakes[0])
-        return self.choose_move(moves)
+        # Setup current grid
+        grid = np.zeros(self.grid_size)
+        # Add candies to grid
+        for candy in candies:
+            grid[candy[0], candy[1]] = 1
+        # Convolve grid with candy kernel
+        candy_conv_grid = ndimage.convolve(grid, self.candy_kernel, mode='constant', cval=0.0)
 
-    def _determine_possible_moves(self, snake, other_snake) -> List[Move]:
+        # Add self to grid
+        for part in snake:
+            grid[part[0], part[1]] = -10
+        # Add snakes to grid
+        for other_snake in other_snakes:
+            for part in other_snake:
+                grid[part[0], part[1]] = -10
+        snake_conv_grid = ndimage.convolve(grid, self.snake_kernel, mode='constant', cval=0.0)
+
+        next_move = self.pick_hightest_move(candy_conv_grid + snake_conv_grid, snake[0])
+
+        return next_move
+
+    def pick_hightest_move(self, convolved_grid: np.ndarray, snake_head: np.array) -> Move:
         """
-        Return a list with all moves that we want to do. Later we'll choose one from this list randomly. This method
-        will be used during unit-testing
+        Check highest rewarded move
         """
-        # highest priority, a move that is on the grid
+        # Highest priority, a move that is on the grid
         on_grid = [move for move in MOVE_VALUE_TO_DIRECTION
-                   if is_on_grid(snake[0] + MOVE_VALUE_TO_DIRECTION[move], self.grid_size)]
+                   if is_on_grid(snake_head + MOVE_VALUE_TO_DIRECTION[move], self.grid_size)]
         if not on_grid:
-            return list(Move)
+            return Move
 
-        # then avoid collisions with other snakes
-        collision_free = [move for move in on_grid
-                          if is_on_grid(snake[0] + MOVE_VALUE_TO_DIRECTION[move], self.grid_size)
-                          and not collides(snake[0] + MOVE_VALUE_TO_DIRECTION[move], [snake, other_snake])]
-        if collision_free:
-            return collision_free
-        else:
-            return on_grid
+        # Choose hightest rewarded move
+        next_move = choice(on_grid)
+        highest_score = -1
 
-    def choose_move(self, moves: List[Move]) -> Move:
-        """
-        Randomly pick a move
-        """
-        return choice(moves)
+        for move in on_grid:
+            idx = snake_head + MOVE_VALUE_TO_DIRECTION[move]
+            score = convolved_grid[idx[0], idx[1]]
+            if score > highest_score:
+                next_move = move
+                highest_score = score
+
+        return next_move
